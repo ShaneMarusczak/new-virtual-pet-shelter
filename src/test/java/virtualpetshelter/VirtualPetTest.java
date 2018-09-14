27b99230180731b5af0bf3d4b3f73d2d6a981bf9
ShaneMarusczak <marusczaks@gmail.com>
2018-09-14 18:45:49 -0400

    first commit

---
+++ src/test/java/virtualpetshelter/VirtualPetTest.java
@@ -0,0 +1,14 @@
+package virtualpetshelter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VirtualPetTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
